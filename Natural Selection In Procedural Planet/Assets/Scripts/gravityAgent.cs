832cbc48dabac3f02d139f0dfe7c915894df7a11
pablogalve <pablogalve100@gmail.com>
2021-06-23 17:33:48 +0200

    gravity added

---
+++ Natural Selection In Procedural Planet/Assets/Scripts/gravityAgent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityAgent : MonoBehaviour
+{
+    public float distanceToCenter;
+    public GameObject planet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planet = GameObject.FindWithTag("Planet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToCenter = GetDistance(gameObject, planet);
+    }
+
+    float GetDistance(GameObject obj1, GameObject obj2)
+    {
+        return Vector3.Distance(obj1.transform.position, obj2.transform.position);
+    }
+}
---
+++ Natural Selection In Procedural Planet/Assets/Scripts/gravityAttractor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityAttractor : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    const float G = 667.4f;
+
+    void FixedUpdate()
+    {
+        GameObject[] animals = GameObject.FindGameObjectsWithTag("Animal");
+        foreach (GameObject animal in animals)
+        {
+            if (animal != this)
+                Attract(animal);
+        }
+    }
+
+    void Attract(GameObject objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.GetComponent<Rigidbody>();
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
