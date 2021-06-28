e26e62b7208ac0d948986eaebfb1a75c1b3dd1f6
pablogalve <pablogalve100@gmail.com>
2021-06-28 20:48:14 +0200

    herviboreAI - area vision detecting close objects

--- Natural Selection In Procedural Planet/Assets/Scripts/animalAI.cs
+++ Natural Selection In Procedural Planet/Assets/Scripts/animalAI.cs
@@ -7,6 +7,10 @@ public class animalAI : MonoBehaviour
     public float speed = 3.0f;
     public float hunger = 100.0f;
 
+    protected List<GameObject> visibleCarnivores = new List<GameObject>();
+    protected List<GameObject> visibleHervibores = new List<GameObject>();
+    protected List<GameObject> visibleFood = new List<GameObject>();
+
     protected virtual void Update()
     {
         Move();
@@ -18,11 +22,47 @@ public class animalAI : MonoBehaviour
         transform.position += transform.forward * speed * Time.deltaTime;
     }
 
-    void UpdateStats()
+    protected virtual void UpdateStats()
     {
         hunger -= Time.deltaTime;
 
         if (hunger <= 0.0f)
             Destroy(this.gameObject);
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        var multiTag = other.gameObject.GetComponent<CustomTag>();
+
+        {   //Keep track of objects inside area of vision
+            if (other.gameObject.tag == "Food")
+                visibleFood.Add(other.gameObject);
+
+            if (multiTag != null)
+            {
+                if (multiTag.HasTag("Carnivore"))
+                    visibleCarnivores.Add(other.gameObject);
+                else if (multiTag.HasTag("Hervibore"))
+                    visibleHervibores.Add(other.gameObject);
+            }
+        }
+    }
+
+    protected virtual void OnTriggerExit(Collider other)
+    {
+        var multiTag = other.gameObject.GetComponent<CustomTag>();
+
+        {   //Keep track of objects inside area of vision
+            if (other.gameObject.tag == "Food")
+                visibleFood.Remove(other.gameObject);
+
+            if (multiTag != null)
+            {
+                if (multiTag.HasTag("Carnivore"))
+                    visibleCarnivores.Remove(other.gameObject);
+                else if (multiTag.HasTag("Hervibore"))
+                    visibleHervibores.Remove(other.gameObject);
+            }
+        }
     }
 }
--- Natural Selection In Procedural Planet/Assets/Scripts/herviboreAI.cs
+++ Natural Selection In Procedural Planet/Assets/Scripts/herviboreAI.cs
@@ -8,18 +8,20 @@ public class herviboreAI : animalAI
     protected override void Update()
     {
         base.Update();
-        UpdateStats();
     }
 
-    void UpdateStats()
+    protected override void UpdateStats()
     {
-
+        Debug.Log("Food in view: " + visibleFood.Count);
+        //Debug.Log("Hervibores in view: " + visibleHervibores.Count);
+        //Debug.Log("Carnivores in view: " + visibleCarnivores.Count);
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Food")
         {
+            visibleFood.Remove(other.gameObject);
             hunger += DataHolder.hungerIncrementAtEating;
             DataHolder.food.Remove(other.gameObject);
             Destroy(other.gameObject);
@@ -27,8 +29,10 @@ public class herviboreAI : animalAI
     }
 
     //Vision
-    void OnTriggerEnter(Collider other)
+    protected override void OnTriggerEnter(Collider other)
     {
+        base.OnTriggerEnter(other);
+
         if (other.gameObject.tag == "Food")
         {
             transform.LookAt(other.gameObject.transform);
@@ -42,8 +46,10 @@ public class herviboreAI : animalAI
         }
     }
 
-    void OnTriggerExit(Collider other)
+    protected override void OnTriggerExit(Collider other)
     {
+        base.OnTriggerExit(other);
+
         if (other.gameObject.tag == "Food")
         {
             transform.LookAt(other.gameObject.transform);
