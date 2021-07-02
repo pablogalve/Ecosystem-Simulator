3d172e618365c7ba773a8c464552b522872abbd9
pablogalve <pablogalve100@gmail.com>
2021-07-02 17:59:04 +0200

    optimize killEntity method

--- Natural Selection In Procedural Planet/Assets/Scripts/animalsAI/EntityManager.cs
+++ Natural Selection In Procedural Planet/Assets/Scripts/animalsAI/EntityManager.cs
@@ -41,88 +41,129 @@ public class EntityManager : MonoBehaviour
     public static void KillEntity(GameObject killedObj)
     {
         killedObj.GetComponent<entity>().toDelete = true;
-        RemoveToDeleteObjectsFromLists();
+
+        var multiTag = killedObj.gameObject.GetComponent<CustomTag>();
+
+        if (killedObj.tag == "Food")
+            RemoveFoodFromLists();
+
+        if (multiTag != null)
+        {
+            if (multiTag.HasTag("Herbivores"))
+                RemoveHerbivoresFromLists();
+            if (multiTag.HasTag("Omnivores"))
+                RemoveOmnivoresFromLists();
+            if (multiTag.HasTag("Carnivores"))
+                RemoveCarnivoresFromLists();
+        }
+
         Destroy(killedObj);
     }
 
-    private static void RemoveToDeleteObjectsFromLists()
+    private static void RemoveFoodFromLists()
     {
-        //Remove deleted objects from visibleAreas
+        //Remove food from food list
+        for (var i = food.Count - 1; i > -1; i--)
         {
-            //Remove all objects from herbivores visible area
+            if (food[i].GetComponent<entity>().toDelete)
+                food.RemoveAt(i);
+        }
+
+        if (herbivores != null)
+        {
             for (var i = herbivores.Count - 1; i > -1; i--)
             {
                 //Remove food from herbivores visible area
-                for (var j = herbivores[i].GetComponent<animalAI>().visibleFood.Count - 1; j > -1; j--)
+                if (herbivores[i] != null)
                 {
-                    if (herbivores[i].GetComponent<animalAI>().visibleFood[j].GetComponent<entity>().toDelete)
-                        herbivores[i].GetComponent<animalAI>().visibleFood.RemoveAt(j);
-                }
-
-                //Remove other herbivores from herbivores visible area
-                for (var j = herbivores[i].GetComponent<animalAI>().visibleHerbivores.Count - 1; j > -1; j--)
-                {
-                    if (herbivores[i].GetComponent<animalAI>().visibleHerbivores[j].GetComponent<entity>().toDelete)
-                        herbivores[i].GetComponent<animalAI>().visibleHerbivores.RemoveAt(j);
-                }
-
-                //Remove carnivores from herbivores visible area
-                for (var j = herbivores[i].GetComponent<animalAI>().visibleCarnivores.Count - 1; j > -1; j--)
-                {
-                    if (herbivores[i].GetComponent<animalAI>().visibleCarnivores[j].GetComponent<entity>().toDelete)
-                        herbivores[i].GetComponent<animalAI>().visibleCarnivores.RemoveAt(j);
-                }
-            }
-
-            //Remove objects from carnivores visible area
-            for (var i = carnivores.Count - 1; i > -1; i--)
-            {
-                if (carnivores[i].GetComponent<animalAI>() == null)
-                    Debug.Log("animalAI component in a carnivore is NULL");
-                //Remove food from carnivores visible area
-                for (var j = carnivores[i].GetComponent<animalAI>().visibleFood.Count - 1; j > -1; j--)
-                {
-                    if (carnivores[i].GetComponent<animalAI>().visibleFood[j].GetComponent<entity>().toDelete)
-                        carnivores[i].GetComponent<animalAI>().visibleFood.RemoveAt(j);
-                }
-
-                //Remove herbivores from carnivores visible area
-                for (var j = carnivores[i].GetComponent<animalAI>().visibleHerbivores.Count - 1; j > -1; j--)
-                {
-                    if (carnivores[i].GetComponent<animalAI>().visibleHerbivores[j].GetComponent<entity>().toDelete)
-                        carnivores[i].GetComponent<animalAI>().visibleHerbivores.RemoveAt(j);
-                }
-
-                //Remove carnivores from carnivores visible area
-                for (var j = carnivores[i].GetComponent<animalAI>().visibleCarnivores.Count - 1; j > -1; j--)
-                {
-                    if (carnivores[i].GetComponent<animalAI>().visibleCarnivores[j].GetComponent<entity>().toDelete)
-                        carnivores[i].GetComponent<animalAI>().visibleCarnivores.RemoveAt(j);
+                    for (var j = herbivores[i].GetComponent<animalAI>().visibleFood.Count - 1; j > -1; j--)
+                    {
+                        if (herbivores[i].GetComponent<animalAI>().visibleFood[j].GetComponent<entity>().toDelete)
+                            herbivores[i].GetComponent<animalAI>().visibleFood.RemoveAt(j);
+                    }
                 }
             }
         }
 
-        //Objects list
+        if (carnivores != null)
         {
-            //Remove herbivores from hervbivores list
-            for (var i = herbivores.Count - 1; i >= 0; i--)
+            for (var i = carnivores.Count - 1; i > -1; i--)
             {
-                if (herbivores[i].GetComponent<entity>().toDelete)
-                    herbivores.RemoveAt(i);
+                //Remove food from carnivores visible area
+                if (carnivores[i] != null)
+                {
+                    for (var j = carnivores[i].GetComponent<animalAI>().visibleFood.Count - 1; j > -1; j--)
+                    {
+                        if (carnivores[i].GetComponent<animalAI>().visibleFood[j].GetComponent<entity>().toDelete)
+                            carnivores[i].GetComponent<animalAI>().visibleFood.RemoveAt(j);
+                    }
+                }
             }
+        }
 
-            //Remove food from food list
-            for (var i = food.Count - 1; i > -1; i--)
+    }
+
+    private static void RemoveHerbivoresFromLists()
+    {
+        //Remove herbivores from hervbivores list
+        for (var i = herbivores.Count - 1; i >= 0; i--)
+        {
+            if (herbivores[i].GetComponent<entity>().toDelete)
+                herbivores.RemoveAt(i);
+        }
+
+        for (var i = herbivores.Count - 1; i > -1; i--)
+        {
+            //Remove other herbivores from herbivores visible area
+            for (var j = herbivores[i].GetComponent<animalAI>().visibleHerbivores.Count - 1; j > -1; j--)
             {
-                if (food[i].GetComponent<entity>().toDelete)
-                    food.RemoveAt(i);
+                if (herbivores[i].GetComponent<animalAI>().visibleHerbivores[j].GetComponent<entity>().toDelete)
+                    herbivores[i].GetComponent<animalAI>().visibleHerbivores.RemoveAt(j);
             }
+        }
 
-            //Remove carnivores from carnivores list
-            for (var i = herbivores.Count - 1; i >= 0; i--)
+        for (var i = carnivores.Count - 1; i > -1; i--)
+        {
+            //Remove herbivores from carnivores visible area
+            for (var j = carnivores[i].GetComponent<animalAI>().visibleHerbivores.Count - 1; j > -1; j--)
             {
-                if (herbivores[i].GetComponent<entity>().toDelete)
-                    herbivores.RemoveAt(i);
+                if (carnivores[i].GetComponent<animalAI>().visibleHerbivores[j].GetComponent<entity>().toDelete)
+                    carnivores[i].GetComponent<animalAI>().visibleHerbivores.RemoveAt(j);
+            }
+        }
+    }
+
+    private static void RemoveOmnivoresFromLists()
+    {
+
+    }
+
+    private static void RemoveCarnivoresFromLists()
+    {
+        //Remove carnivores from carnivores list
+        for (var i = herbivores.Count - 1; i >= 0; i--)
+        {
+            if (herbivores[i].GetComponent<entity>().toDelete)
+                herbivores.RemoveAt(i);
+        }
+
+        for (var i = herbivores.Count - 1; i > -1; i--)
+        {
+            //Remove carnivores from herbivores visible area
+            for (var j = herbivores[i].GetComponent<animalAI>().visibleCarnivores.Count - 1; j > -1; j--)
+            {
+                if (herbivores[i].GetComponent<animalAI>().visibleCarnivores[j].GetComponent<entity>().toDelete)
+                    herbivores[i].GetComponent<animalAI>().visibleCarnivores.RemoveAt(j);
+            }
+        }
+
+        for (var i = carnivores.Count - 1; i > -1; i--)
+        {
+            //Remove carnivores from carnivores visible area
+            for (var j = carnivores[i].GetComponent<animalAI>().visibleCarnivores.Count - 1; j > -1; j--)
+            {
+                if (carnivores[i].GetComponent<animalAI>().visibleCarnivores[j].GetComponent<entity>().toDelete)
+                    carnivores[i].GetComponent<animalAI>().visibleCarnivores.RemoveAt(j);
             }
         }
     }
