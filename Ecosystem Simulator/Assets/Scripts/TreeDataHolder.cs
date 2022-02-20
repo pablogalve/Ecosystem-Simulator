3aa4390437a19d5d35e8ff75fe04e617e670a38a
Pablo Galve <pablogalve100@gmail.com>
2022-02-20 15:52:24 +0100

    Tree basic system of food generation, growth and reproduction

---
+++ Ecosystem Simulator/Assets/Scripts/TreeDataHolder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeDataHolder : MonoBehaviour
+{
+    public static float maxAge = 60.0f;
+    public static float foodGenerationFreq = 30.0f;
+    public static float reproductionFreq = 20.0f;
+
+    public static int amountOfTrees = 0;    
+}
