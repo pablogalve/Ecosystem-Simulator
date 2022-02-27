1c741a65078771204dcbefe72fe6605ddd653401
Pablo Galve <pablogalve100@gmail.com>
2022-02-27 19:56:31 +0100

    Create entity and EntityManager classes

---
+++ Ecosystem Simulator/Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    // AGE
+    public byte age; // 0 - 255
+    private string uuid;
+
+    void Start()
+    {
+        age = 0;
+        SetUUID();
+    }
+
+    private void SetUUID()
+    {
+        uuid = System.Guid.NewGuid().ToString();
+    }
+
+    public string GetUUID()
+    {
+        return uuid;
+    }
+}
---
+++ Ecosystem Simulator/Assets/Scripts/Entities/EntityManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityManager : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    public void TryToSpawn()
+    {
+
+    }
+
+    public void TryToKill()
+    {
+
+    }
+}
