ef1a73168e90b63c3051c23aa85dff98f7b832e3
Pablo Galve <pablogalve100@gmail.com>
2022-06-19 02:52:51 +0200

    Move settings to a separate C# script

---
+++ Ecosystem Simulator/Assets/Scripts/Settings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+    }
+}
