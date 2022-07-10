db08d8bbb172b0e9a7b560915ae916732481fc63
Pablo Galve <pablogalve100@gmail.com>
2022-07-10 17:50:47 +0200

    Update settings

--- Ecosystem Simulator/Assets/Scripts/Entities/EntityManager.cs
+++ Ecosystem Simulator/Assets/Scripts/Entities/EntityManager.cs
@@ -118,7 +118,7 @@ public class EntityManager : MonoBehaviour
                 entityFactory.SpawnRandomTree(1000f, 1000f, 1000f, 40);
             }
 
-            for (int i = 0; i < 200; i++)
+            for (int i = 0; i < 350; i++)
             {
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.LONGHORN, 500f, 500f, 50f);
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.LONGHORN, 600f, 600f, 50f);
@@ -126,7 +126,7 @@ public class EntityManager : MonoBehaviour
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.LONGHORN, 800f, 800f, 50f);
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.LONGHORN, 1200f, 1200f, 50f);
             }
-            for (int i = 0; i < 200; i++)
+            for (int i = 0; i < 350; i++)
             {
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.SHEEP, 500f, 1200f, 100f);
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.SHEEP, 700f, 700f, 100f);
@@ -134,7 +134,7 @@ public class EntityManager : MonoBehaviour
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.SHEEP, 500f, 1200f, 100f);
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.SHEEP, 700f, 600f, 100f);
             }
-            for (int i = 0; i < 200; i++)
+            for (int i = 0; i < 350; i++)
             {
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.WOLF, 600f, 600f, 100f);
                 entityFactory.SpawnAnimalOfRandomGenderAndAge((int)AnimalManager.Species.WOLF, 700f, 700f, 100f);
